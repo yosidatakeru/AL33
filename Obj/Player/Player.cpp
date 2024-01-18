9b8530b4350a350a09ab9fa340260ffb7ba1f4f4
ヨシダタケル <115062870+yosidatakeru@users.noreply.github.com>
2024-01-18 11:29:45 +0900

    Add files via upload

---
+++ Obj/Player/Player.cpp
@@ -0,0 +1,132 @@
+﻿#include "Obj/Player/Player.h"
+#include "Function.h"
+#include <ImguiManager.h>
+#include <cassert>
+#include "GameScene.h"
+
+
+
+
+
+
+void Player::Initialize(Model* model, uint32_t textureHandle) {
+
+	assert(model);
+
+	// 引数として受け取ったデータをメンバ変数に記録する
+	this->model_ = model;
+	this->textureHandle_ = textureHandle;
+	// ワールド変数の初期化
+	worldTransform_.Initialize();
+
+	// シングルトンインスタンスを取得
+	//input_ = Input::GetInstance();
+
+
+	
+}
+
+void Player::InitializeFloatingGimmick() 
+{ 
+  floatingParameter_ = 0.0f;
+
+}
+
+
+
+
+
+
+void Player::Update() 
+{
+	////行列を定数バッファに転送する
+	worldTransform_.TransferMatrix();
+	
+	const float kCharacterSpeed = 0.3f;
+	
+	// キャラクターも異動ベクトル
+	Vector3 move = {0.0f, 0.0f, 0.0f};
+
+	XINPUT_STATE joyState;
+	if (Input::GetInstance()->GetJoystickState(0, joyState)) 
+	{
+		move.x += (float)joyState.Gamepad.sThumbLX / SHRT_MAX * kCharacterSpeed;
+		move.z += (float)joyState.Gamepad.sThumbLY / SHRT_MAX * kCharacterSpeed;
+	}
+	//正規化する
+	move = Normalize(move) * kCharacterSpeed;
+
+
+	
+	//回転	
+		Matrix4x4 matRot = MakeRotateXYZMatrix
+		(
+		    viewProjection_->rotation_.x, viewProjection_->rotation_.y,
+		    viewProjection_->rotation_.z);
+
+		move = TransformNormal(move, matRot);
+		
+	
+
+	
+
+	//移動ベクトルのY軸周り角度
+	worldTransform_.rotation_.y = std::atan2(move.x, move.z);
+	
+	
+
+	// 座標移動（ベクトルの加算）
+	worldTransform_.translation_ = Add(worldTransform_.translation_, move);
+	////平行移動行列
+
+	worldTransform_.matWorld_ = MakeAffineMatrix(
+	    worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
+
+	worldTransform_.TransferMatrix();
+
+
+
+
+
+
+	// 画面に座標を出す
+	ImGui::Begin("Player");
+
+	ImGui::InputFloat3("PlayerPosition", &worldTransform_.translation_.x);
+	ImGui::SliderFloat3("PlayerSlide", &worldTransform_.translation_.x, -20.0f, 30.0f);
+	
+	
+
+
+	ImGui::End();
+}
+
+void Player::UpdateFloatingGimmick() 
+{
+
+
+}
+
+
+
+
+void Player::Draw( ViewProjection& viewProjection)
+{
+	model_->Draw(worldTransform_, viewProjection);
+}
+
+
+
+
+Vector3 Player::GetWorldPosition()
+{
+	Vector3 worldPos;
+
+	// ワールド行列の「平行移動成分」を取得(ワールド座標)
+	worldPos.x = worldTransform_.matWorld_.m[3][0];
+	worldPos.y = worldTransform_.matWorld_.m[3][1];
+	worldPos.z = worldTransform_.matWorld_.m[3][2];
+
+	return worldPos;
+}
+
---
+++ Obj/Player/Player.h
@@ -0,0 +1,83 @@
+﻿#pragma once
+#include "Model.h"
+#include "WorldTransform.h"
+#include "Function.h"
+#include "Input.h"
+#include"PlayerBullet.h"
+#include<list>
+class Player {
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	
+	/// <summary>
+	/// 自キャラ
+	/// </summary>
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize(Model* model, uint32_t textureHandle);
+
+	void InitializeFloatingGimmick();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	void UpdateFloatingGimmick();
+
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw(ViewProjection& viewProjection);
+
+	
+
+
+	void SetViewProjection(const ViewProjection* viewProjectio) 
+	{
+		 viewProjection_ = viewProjectio;
+	}
+
+	Vector3 GetWorldPosition();
+
+	
+
+	const WorldTransform& GetWorldTransform() { return worldTransform_; }
+
+	
+
+
+private:
+
+	// モデルデータ
+	Model* modelFighterBody_;
+	Model* modelFighterHead_;
+	Model* modelFighterL_arm_;
+	Model* modelFighterR_arm_;
+
+	//ギミックパラメータ
+	float floatingParameter_ = 0.0f;
+
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+
+	
+	Model* model_ = nullptr;
+	uint32_t textureHandle_ = 0u;
+	
+	// キーボード入力
+	Input* input_ = nullptr;
+
+	//カメラ
+	const ViewProjection* viewProjection_ = nullptr;
+
+	/* ----- GamePad ゲームパッド ----- */
+
+	// ゲームパッドの状態を得る変数(XINPUT)
+	XINPUT_STATE joyState_;
+};
